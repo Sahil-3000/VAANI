f94a1e7a7def8efbae1110d5a414ad896f39f442
Sahil <danksahil895@gmail.com>
2025-02-01 18:37:00 +0530

    updates

--- JS Files/podcasts.js
+++ JS Files/podcasts.js
@@ -14,6 +14,150 @@ let podcasts = [
                 cover: "covers/podcasts/THP/10 Dialects Of Haryana.jpg",
                 audio: "https://www.dropbox.com/scl/fi/mp6l1u151j0ixiszeb05b/10_Dialects_of_Haryana___Haryanvi_Talk-128k-1.m4a?rlkey=5bomkosmqkkuv00agelrk3ai9&st=su7zvair&dl=1",
                 searchString: "10 Dialects of Haryanva The Haryanvi Podcast THP"
+            },
+            {
+                title: "A Script Of Haryanvi Language",
+                artist: "Nishant Dahiya",
+                lyrics: "A Script Of Haryanvi Language",
+                artistPhoto: "covers/podcasts/THP/Nishant Dahiya.png",
+                cover: "covers/podcasts/THP/A Script Of Haryanvi Language.jpg",
+                audio: "https://www.dropbox.com/scl/fi/arpvpvn5v2jpzj30uk0zj/A_Script_for_Haryanvi_Language___Haryanvi_Talk-128k-1.m4a?rlkey=j9hmri8wv3nep2gmm7701wcug&st=98j6849s&dl=1",
+                searchString: "A Script Of Haryanvi Language The Haryanvi Podcast THP"
+            },
+            {
+                title: "Ancient Haryana History Masterclass",
+                artist: "Nishant Dahiya",
+                lyrics: "Ancient Haryana History Masterclass",
+                artistPhoto: "covers/podcasts/THP/Nishant Dahiya.png",
+                cover: "covers/podcasts/THP/Ancient Haryana History Masterclass.jpg",
+                audio: "https://www.dropbox.com/scl/fi/xzyb45katlrnvkhnuqafp/Ancient_Haryana_History_Masterclass___The_Haryanvi_Podcast-128k-1.m4a?rlkey=o7ded39c988iy737am7sorui6&st=l3iif60b&dl=1",
+                searchString: "Ancient Haryana History Masterclass The Haryanvi Podcast THP"
+            },
+            {
+                title: "Best Books On Haryana",
+                artist: "Nishant Dahiya",
+                lyrics: "Best Books On Haryana",
+                artistPhoto: "covers/podcasts/THP/Nishant Dahiya.png",
+                cover: "covers/podcasts/THP/Best Books On Haryana.jpg",
+                audio: "https://www.dropbox.com/scl/fi/b4yig6t4rq6eii4u68uu7/Best_Books_on_Haryana___Haryanvi_Talk-128k-1.m4a?rlkey=sgaczetbj1lkwhd6h82xg00lo&st=yn31tude&dl=1",
+                searchString: "Best Books On Haryana The Haryanvi Podcast THP"
+            },
+            {
+                title: "Best Haryanvi Kisse Kahani",
+                artist: "Nishant Dahiya",
+                lyrics: "Best Haryanvi Kisse Kahani",
+                artistPhoto: "covers/podcasts/THP/Nishant Dahiya.png",
+                cover: "covers/podcasts/THP/Best Haryanvi Kisse Kahani.jpg",
+                audio: "https://www.dropbox.com/scl/fi/c3g6jatgchq53yc161892/Best_Haryanvi_Kisse_Kahani___Haryanvi_Talk-128k-1.m4a?rlkey=idhjhd4ppp1caih2r5z864v5s&st=66t31qru&dl=1",
+                searchString: "Best Haryanvi Kisse Kahani The Haryanvi Podcast THP"
+            },
+            {
+                title: "Best Haryanvi Ragni Ever",
+                artist: "Nishant Dahiya",
+                lyrics: "Best Haryanvi Ragni Ever",
+                artistPhoto: "covers/podcasts/THP/Nishant Dahiya.png",
+                cover: "covers/podcasts/THP/Best Haryanvi Ragni Ever.jpg",
+                audio: "https://www.dropbox.com/scl/fi/rc92yr3skhzzcqw4aif16/Best_Haryanvi_Ragni_Ever__Haryanvi_Talk-128k-1.m4a?rlkey=pxz6cztlhc5c7my3ofg274xaz&st=4heq1toc&dl=1",
+                searchString: "Best Haryanvi Ragni Ever The Haryanvi Podcast THP"
+            },
+            {
+                title: "Dada Lakhmi Movie Review",
+                artist: "Nishant Dahiya",
+                lyrics: "Dada Lakhmi Movie Review",
+                artistPhoto: "covers/podcasts/THP/Nishant Dahiya.png",
+                cover: "covers/podcasts/THP/Dada Lakhmi Movie Review.jpg",
+                audio: "https://www.dropbox.com/scl/fi/0mpiz7wg6kmyt84wkw4wg/Dada_Lakhmi_Movie_Review____The_Haryanvi_Podcast-128k-1.m4a?rlkey=74i345z4dd18he974wkf9qc32&st=wnusk2mu&dl=1",
+                searchString: "Dada Lakhmi Movie Review The Haryanvi Podcast THP"
+            },
+            {
+                title: "Haryanvi Singer Sahil Dhull Interview",
+                artist: "Nishant Dahiya",
+                lyrics: "Haryanvi Singer Sahil Dhull Interview",
+                artistPhoto: "covers/podcasts/THP/Nishant Dahiya.png",
+                cover: "covers/podcasts/THP/Haryanvi Singer Sahil Dhull Interview.jpg",
+                audio: "https://www.dropbox.com/scl/fi/qi7tjmw9qyqwx8i9syb5f/Haryanvi_Singer_Sahil_Dhull_Interview___The_Haryanvi_Podcast-128k-1.m4a?rlkey=2uw5b3me3w69m2okp6tkm3ypn&st=nzhcgzky&dl=1",
+                searchString: "Haryanvi Singer Sahil Dhull Interview The Haryanvi Podcast THP"
+            },
+            {
+                title: "Haryanvi Women And Folk Songs",
+                artist: "Nishant Dahiya",
+                lyrics: "Haryanvi Women And Folk Songs",
+                artistPhoto: "covers/podcasts/THP/Nishant Dahiya.png",
+                cover: "covers/podcasts/THP/Haryanvi Women And Folk Songs.jpg",
+                audio: "https://www.dropbox.com/scl/fi/6plqxgdpa4i7vf128xief/Haryanvi_Women_and_Folk_Songs___The_Haryanvi_Podcast-128k-1.m4a?rlkey=ia5i1b5q1ss579vtk7hmnvypf&st=8yjmovtk&dl=1",
+                searchString: "Haryanvi Women And Folk Songs The Haryanvi Podcast THP"
+            },
+            {
+                title: "History Of Haryanvi Clothes",
+                artist: "Nishant Dahiya",
+                lyrics: "History Of Haryanvi Clothes",
+                artistPhoto: "covers/podcasts/THP/Nishant Dahiya.png",
+                cover: "covers/podcasts/THP/History Of Haryanvi Clothes.jpg",
+                audio: "https://www.dropbox.com/scl/fi/o6k3bgi255sxltlj4h3lq/History_of_Haryanvi_Clothes___Haryanvi_Talk-128k-1.m4a?rlkey=lg8x71d6kx80tnmu483zd0b9e&st=4bi28rfc&dl=1",
+                searchString: "History Of Haryanvi Clothes The Haryanvi Podcast THP"
+            },
+            {
+                title: "Language Status Of Haryanvi",
+                artist: "Nishant Dahiya",
+                lyrics: "Language Status Of Haryanvi",
+                artistPhoto: "covers/podcasts/THP/Nishant Dahiya.png",
+                cover: "covers/podcasts/THP/Language Status Of Haryanvi.jpg",
+                audio: "https://www.dropbox.com/scl/fi/ntyxoyhy6u0awcx9cs79t/Language_Status_for_Haryanvi___The_Haryanvi_Podcast-128k-1.m4a?rlkey=vi3jvgs8qsabrrkab77mez5ox&st=yghjepui&dl=1",
+                searchString: "Language Status Of Haryanvi The Haryanvi Podcast THP"
+            },
+            {
+                title: "Manoj Babli Honuor Killing",
+                artist: "Nishant Dahiya",
+                lyrics: "Manoj Babli Honuor Killing",
+                artistPhoto: "covers/podcasts/THP/Nishant Dahiya.png",
+                cover: "covers/podcasts/THP/Manoj Babli Honuor Killing.jpg",
+                audio: "https://www.dropbox.com/scl/fi/kwgkekpd86mq2cjlgwcp0/Manoj_Babli_Honour_Killing___Haryanvi_Talk-128k-1.m4a?rlkey=geaevc6rmbogql5474tp5fd6r&st=v1apt8ci&dl=1",
+                searchString: "Manoj Babli Honuor Killing The Haryanvi Podcast THP"
+            },
+            {
+                title: "Paani Aali Paani Pyade Ragni",
+                artist: "Nishant Dahiya",
+                lyrics: "Paani Aali Paani Pyade Ragni",
+                artistPhoto: "covers/podcasts/THP/Nishant Dahiya.png",
+                cover: "covers/podcasts/THP/Paani Aali Paani Pyade Ragni.jpg",
+                audio: "https://www.dropbox.com/scl/fi/t68k3rc8uccvrjffvgo62/Paani_Aali_Pani_Pyade_Ragni___Haryana_Literary_Festival-128k-1.m4a?rlkey=v8ub2hi0pbuhh3s8lfbi9we3f&st=7v5exz4h&dl=1",
+                searchString: "Paani Aali Paani Pyade Ragni The Haryanvi Podcast THP"
+            },
+            {
+                title: "Soft Skills For Haryanvi Youth",
+                artist: "Nishant Dahiya",
+                lyrics: "Soft Skills For Haryanvi Youth",
+                artistPhoto: "covers/podcasts/THP/Nishant Dahiya.png",
+                cover: "covers/podcasts/THP/Soft Skills For Haryanvi Youth.jpg",
+                audio: "https://www.dropbox.com/scl/fi/825yf7kpissmb4b0oqtos/Soft_Skills_for_Haryanvi_Youth___Haryana_Literary_Festival-128k-1.m4a?rlkey=qduxczlq1ahikauwlvhtjo7q1&st=a7ov7u0w&dl=1",
+                searchString: "Soft Skills For Haryanvi Youth The Haryanvi Podcast THP"
+            },
+            {
+                title: "The Journey Of Stage App",
+                artist: "Nishant Dahiya",
+                lyrics: "The Journey Of Stage App",
+                artistPhoto: "covers/podcasts/THP/Nishant Dahiya.png",
+                cover: "covers/podcasts/THP/The Journey Of Stage App.jpg",
+                audio: "https://www.dropbox.com/scl/fi/yc8uzu607rr0ve5kr5ul4/The_Journey_of_STAGE_App___Haryanvi_Talk-128k-1.m4a?rlkey=dkhvxcn5dieu3m6pmg13tqj5q&st=y7tc0agq&dl=1",
+                searchString: "The Journey Of Stage App The Haryanvi Podcast THP"
+            },
+            {
+                title: "The Origin Of Haryanvi Langauge",
+                artist: "Nishant Dahiya",
+                lyrics: "The Origin Of Haryanvi Langauge",
+                artistPhoto: "covers/podcasts/THP/Nishant Dahiya.png",
+                cover: "covers/podcasts/THP/The Origin Of Haryanvi Langauge.jpg",
+                audio: "https://www.dropbox.com/scl/fi/b4psmefpxpuar7i3mtqi3/The_Origin_of_Haryanvi_Language___Haryanvi_Talk-128k-1.m4a?rlkey=xwdzuwjmt4hoki6560u6b99w2&st=0mtg4lhi&dl=1",
+                searchString: "The Origin Of Haryanvi Langauge The Haryanvi Podcast THP"
+            },
+            {
+                title: "Women Caste And Haryana",
+                artist: "Nishant Dahiya",
+                lyrics: "Women Caste And Haryana",
+                artistPhoto: "covers/podcasts/THP/Nishant Dahiya.png",
+                cover: "covers/podcasts/THP/Women Caste And Haryana.jpg",
+                audio: "https://www.dropbox.com/scl/fi/bpkg7gqhc50i2v4qcjzzj/Women_Caste_and_Haryana___The_Haryanvi_Podcast-128k-1.m4a?rlkey=wxfq1z9b6aa5hfj8zrm3hz5fq&st=n4jvfm0y&dl=1",
+                searchString: "Women Caste And Haryana The Haryanvi Podcast THP"
             }
         ]
     },
@@ -44,22 +188,57 @@ let podcasts = [
             }
         ]
     },
-
     {
         id: 3,
-        title: "The Haryanvi podcast",
-        artist: "Nishant Dahiya",
-        description: "The Haryanvi Podcast is your one stop destination for all things Haryanvi. We talk about Haryanvi Music, Culture, Society and much more here in Haryanvi Language.",
-        cover: "covers/podcasts/THP/THP.png",
+        title: "Huberman Lab Essentials",
+        artist: "Andrew Huberman",
+        description: "The Huberman Lab podcast is hosted by Andrew Huberman, Ph.D., a neuroscientist and tenured professor in the department of neurobiology, and by courtesy, psychiatry and behavioral sciences at Stanford School of Medicine. The podcast discusses neuroscience and science-based tools, including how our brain and its connections with the organs of our body control our perceptions, our behaviors, and our health......",
+        cover: "covers/podcasts/HLE/HLE.jpg",
         songs: [
             {
-                title: "10 Dialects of Haryanva",
-                artist: "Nishant Dahiya",
-                lyrics: "10 Dialects of Haryanva",
-                artistPhoto: "covers/podcasts/THP/Nishant Dahiya.png",
-                cover: "covers/podcasts/THP/10 Dialects Of Haryana.jpg",
-                audio: "https://www.dropbox.com/scl/fi/mp6l1u151j0ixiszeb05b/10_Dialects_of_Haryana___Haryanvi_Talk-128k-1.m4a?rlkey=5bomkosmqkkuv00agelrk3ai9&st=su7zvair&dl=1",
-                searchString: "10 Dialects of Haryanva The Haryanvi Podcast THP"
+                title: "ADHD And Improving Focus",
+                artist: "Andrew Huberman",
+                lyrics: "ADHD And Improving Focus",
+                artistPhoto: "covers/podcasts/HLE/Andrew Huberman.jpg",
+                cover: "covers/podcasts/HLE/ADHD And Improving Focus.jpg",
+                audio: "https://www.dropbox.com/scl/fi/xyz123/ADHD_And_Improving_Focus.m4a?rlkey=abc123&st=xyz&dl=1",
+                searchString: "ADHD And Improving Focus Huberman Lab Essentials"
+            },
+            {
+                title: "Build Inner Strength",
+                artist: "Andrew Huberman",
+                lyrics: "Build Inner Strength",
+                artistPhoto: "covers/podcasts/HLE/Andrew Huberman.jpg",
+                cover: "covers/podcasts/HLE/Build Inner Strength.jpg",
+                audio: "https://www.dropbox.com/scl/fi/xyz123/Build_Inner_Strength.m4a?rlkey=abc123&st=xyz&dl=1",
+                searchString: "Build Inner Strength Huberman Lab Essentials"
+            },
+            {
+                title: "Control Pain And Heal Faster",
+                artist: "Andrew Huberman",
+                lyrics: "Control Pain And Heal Faster",
+                artistPhoto: "covers/podcasts/HLE/Andrew Huberman.jpg",
+                cover: "covers/podcasts/HLE/Control Pain And Heal Faster.jpg",
+                audio: "https://www.dropbox.com/scl/fi/xyz123/Control_Pain_And_Heal_Faster.m4a?rlkey=abc123&st=xyz&dl=1",
+                searchString: "Control Pain And Heal Faster Huberman Lab Essentials"
+            },
+            {
+                title: "Exercise And Brain Health",
+                artist: "Andrew Huberman",
+                lyrics: "Exercise And Brain Health",
+                artistPhoto: "covers/podcasts/HLE/Andrew Huberman.jpg",
+                cover: "covers/podcasts/HLE/Exercise And Brain Health.jpg",
+                audio: "https://www.dropbox.com/scl/fi/xyz123/Exercise_And_Brain_Health.m4a?rlkey=abc123&st=xyz&dl=1",
+                searchString: "Exercise And Brain Health Huberman Lab Essentials"
+            },
+            {
+                title: "How To Guide Your Life Decisions",
+                artist: "Andrew Huberman",
+                lyrics: "How To Guide Your Life Decisions",
+                artistPhoto: "covers/podcasts/HLE/Andrew Huberman.jpg",
+                cover: "covers/podcasts/HLE/How To Guide Your Life Decisions.jpg",
+                audio: "https://www.dropbox.com/scl/fi/xyz123/How_To_Guide_Your_Life_Decisions.m4a?rlkey=abc123&st=xyz&dl=1",
+                searchString: "How To Guide Your Life Decisions Huberman Lab Essentials"
             }
         ]
     },
@@ -90,7 +269,6 @@ let podcasts = [
             }
         ]
     },
-
 
     {
         id: 5,
@@ -207,7 +385,8 @@ function displayPodcastEpisodes(podcast) {
 
         const playButton = document.createElement("button");
         playButton.textContent = "Play";
-        playButton.addEventListener("click", () => {
+        playButton.addEventListener("click", (event) => {
+            event.stopPropagation(); // Prevent triggering the episodeDiv click event
             currentPlaylist = podcast.songs;
             loadTrack(index);
             playSong(episode.audio);
@@ -217,7 +396,8 @@ function displayPodcastEpisodes(podcast) {
         const downloadButton = document.createElement("button");
         downloadButton.textContent = "Download";
         downloadButton.classList.add("download-button");
-        downloadButton.addEventListener("click", () => {
+        downloadButton.addEventListener("click", (event) => {
+            event.stopPropagation(); // Prevent triggering the episodeDiv click event
             const link = document.createElement("a");
             link.href = episode.audio;
             link.download = `${episode.title}.mp3`;
@@ -228,6 +408,12 @@ function displayPodcastEpisodes(podcast) {
         buttonContainer.appendChild(downloadButton);
 
         episodeDiv.appendChild(buttonContainer);
+        episodeDiv.addEventListener("click", () => {
+            currentPlaylist = podcast.songs;
+            loadTrack(index);
+            playSong(episode.audio);
+        });
+
         episodeContainer.appendChild(episodeDiv);
     });
 
