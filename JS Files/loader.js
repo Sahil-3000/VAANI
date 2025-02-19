55cddff08979df3f968c2bdb3391fc268e1e21c1
Sahil <danksahil895@gmail.com>
2025-02-19 16:36:24 +0530

    updates

--- JS Files/loader.js
+++ JS Files/loader.js
@@ -1,29 +1,29 @@
-// const { defaultPLaylist } = require("JS Files/defaultPlaylist.js");
+document.addEventListener("DOMContentLoaded", function() {
+    const loader = document.querySelector(".loader");
+    loader.style.display = "block";
 
-// document.addEventListener("DOMContentLoaded", function() {
-//     const loader = document.querySelector(".loader");
-//     loader.style.display = "block";
+    const images = document.images;
+    const totalImages = images.length;
+    let loadedImages = 0;
 
-//     // Preload audio files from defaultPlaylist
-//     const audioFiles = defaultPlaylist.map(item => item.src);
-//     let loadedCount = 0;
+    function imageLoaded() {
+        loadedImages++;
+        if (loadedImages === totalImages) {
+            loader.style.display = "none";
+        }
+    }
 
-//     audioFiles.forEach(src => {
-//         const audio = new Audio(src);
-//         audio.addEventListener("canplaythrough", () => {
-//             loadedCount++;
-//             if (loadedCount === audioFiles.length) {
-//                 loader.style.display = "none";
-//             }
-//         });
-//     });
+    for (let i = 0; i < totalImages; i++) {
+        if (images[i].complete) {
+            imageLoaded();
+        } else {
+            images[i].addEventListener("load", imageLoaded);
+            images[i].addEventListener("error", imageLoaded);
+        }
+    }
 
-//     // Fallback in case some audio files fail to load
-//     setTimeout(() => {
-//         loader.style.display = "none";
-//     }, 10000); // 10 seconds timeout
-
-//     // window.addEventListener("load", function() {
-//     //     loader.style.display = "none";
-//     // });
-// });
+    // Fallback in case some images fail to load
+    setTimeout(() => {
+        loader.style.display = "none";
+    }, 10000); // 10 seconds timeout
+});
